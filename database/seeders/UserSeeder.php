85f6a55defb974a3e548f0c085e395316462bd91
Dennis <dennisvanstrien2007@Gmail.com>
2025-11-17 11:32:13 +0100

    Hoofdletter

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => Hash::make('password123'),
             'is_admin' => true,
-            'department' => 'management',
+            'department' => 'Management',
         ]);
 
         User::create([
